3c991652744760380fdd14f26dc18d20ad7ccb57
imiebaka <triple07even@gmail.com>
2024-05-30 11:41:32 +0100

    🧪 fixture waring issue fixed

--- core/test/test_main.py
+++ core/test/test_main.py
@@ -13,13 +13,6 @@ engine = Engine
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope="session")
-def event_loop():
-    """Create an instance of the default event loop for the session."""
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
-
 
 @pytest.fixture(scope="session")
 async def async_app_client():
@@ -30,50 +23,49 @@ async def async_app_client():
 
 
 """ Accounting testing """
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_1_create_user(async_app_client):
     await account.test_1_create_user(async_app_client)
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_1_create_users_others(async_app_client):
     await account.test_1_create_users_others(async_app_client)
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_2_create_existing_user(async_app_client):
     await account.test_2_create_existing_user(async_app_client)
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_4_login_user(async_app_client):
     await account.test_4_login_user(async_app_client)
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_5_user_profile(async_app_client):
     await account.test_5_user_profile(async_app_client)
 
 
 
 """ Exhortation """
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_1_create_exhortation(async_app_client):
     await exhortation.test_1_create_exhortation(async_app_client)
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_2_read_exhortation(async_app_client):
     await exhortation.test_2_read_exhortation(async_app_client)
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_3_update_exhortation(async_app_client):
     await exhortation.test_3_update_exhortation(async_app_client)
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_4_delete_exhortation(async_app_client):
     await exhortation.test_1_create_exhortation(async_app_client)
     await exhortation.test_1_create_exhortation(async_app_client)
     await exhortation.test_4_delete_exhortation(async_app_client)
 
 
-
 """ Exhortation comment """
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_1_make_comment(async_app_client):
     await comment.test_1_make_comment(async_app_client)
