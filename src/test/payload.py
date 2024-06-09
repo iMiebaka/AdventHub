10f9ad448bd625390e88bd1a2d0fff1532efbfb0
imiebaka <triple07even@gmail.com>
2024-06-09 20:45:38 +0100

    ✨ comments added

--- src/test/payload.py
+++ src/test/payload.py
@@ -5,22 +5,29 @@ class Payload:
             json.dump([], data, indent=4)
 
     def user_list(self, index):
+        """Pass an integer to get a particular user sample 
+           or pass an empty string to the whole list"""
         with open("src/test/asset/test_users.json", "r") as data:
             if type(index) == int:
                 return json.load(data)[index]
             return json.load(data)
 
     def write_token(self, token):
+        """Pass down a list of valid user token"""
         with open("src/test/asset/auth.json", "w") as data:
             json.dump( token, data, indent=4)
 
     def read_token(self, index):
+        """Pass an integer to get a particular user token
+           or pass an empty string to the whole list"""
         with open("src/test/asset/auth.json", "r") as data:
             if type(index) == int:
                 return json.load(data)[index]
             return json.load(data)
 
     def comment_list(self, index):
+        """Pass an integer to get a particular text comment
+           or pass an empty string to the whole list"""
         with open("src/test/asset/comments.json", "r") as data:
             if type(index) == int:
                 return json.load(data)[index]
